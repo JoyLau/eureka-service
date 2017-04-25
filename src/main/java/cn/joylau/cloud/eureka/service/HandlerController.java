5c094931263de4c6ecdd580e569af3568b2c85f4
LiuFa_work <2587038142@qq.com>
2017-04-25 17:26:31 +0800

    eureka-service

--- src/main/java/cn/joylau/cloud/eureka/service/HandlerController.java
+++ src/main/java/cn/joylau/cloud/eureka/service/HandlerController.java
@@ -4,26 +4,28 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by JoyLau on 4/14/2017.
  * cn.joylau.cloud.eureka.service
  * fa.liu@gtafe.com
  */
-@RestController
+@Controller
 public class HandlerController {
     private final Logger logger = Logger.getLogger(getClass());
+
     @Autowired
     private DiscoveryClient client;
-    @RequestMapping(value = "/add" ,method = RequestMethod.GET)
-    public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
+
+    @RequestMapping(value = "/info" ,method = RequestMethod.GET)
+    @ResponseBody
+    public String add() {
         ServiceInstance instance = client.getLocalServiceInstance();
-        Integer r = a + b;
-        logger.info("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
-        return r;
+        logger.info("/info, host:" + instance.getHost() + ", service_id:" + instance.getServiceId());
+        return "Hello , 您访问的主机是"+ instance.getHost() + instance.getPort() + "service_id是"+instance.getServiceId();
     }
 }
